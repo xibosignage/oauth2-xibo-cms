4eeed7c39cd242848d39f2d74c135fed1e7fb54b
Peter <peter@springsignage.com>
2016-09-20 13:32:11 +0100

    Added playlist and widget wrappers

---
+++ src/Entity/XiboPlaylist.php
@@ -0,0 +1,43 @@
+<?php
+/*
+ * Spring Signage Ltd - http://www.springsignage.com
+ * Copyright (C) 2016 Spring Signage Ltd
+ * (XiboPlaylist.php)
+ */
+
+
+namespace Xibo\OAuth2\Client\Entity;
+
+
+use Xibo\OAuth2\Client\Exception\XiboApiException;
+
+/**
+ * Class XiboPlaylist
+ * @package Xibo\OAuth2\Client\Entity
+ */
+class XiboPlaylist extends XiboEntity
+{
+    public $playlistId;
+    public $ownerId;
+    public $name;
+    public $tags;
+    public $regions;
+    public $widgets;
+    public $permissions;
+    public $displayOrder;
+	
+	/**
+     * Assign media to playlist
+     * @param $playlistMedia
+     * @param $playlistRegion
+     * @return XiboPlaylist
+     */
+    public function assign($playlistMedia, $playlistRegion)
+    {
+        $response = $this->doPost('playlist/library/assign/' . $playlistRegion, [
+        	'media' => [$playlistMedia]
+        	]);
+
+        return $this;
+    }
+}
