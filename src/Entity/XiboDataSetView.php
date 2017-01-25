3860f566831c5fe01d13c68b464c389556d43f0f
Peter <peter@springsignage.com>
2017-01-25 15:53:55 +0000

    Fixed wrappers, added edit/delete

--- src/Entity/XiboDataSetView.php
+++ src/Entity/XiboDataSetView.php
@@ -13,7 +13,50 @@ use Xibo\OAuth2\Client\Exception\XiboApiException;
 
 class XiboDataSetView extends XiboEntity
 {
-	/**
+    public $widgetId;
+    public $playlistId;
+    public $ownerId;
+    public $type;
+    public $duration;
+    public $displayOrder;
+    public $useDuration;
+    public $calculatedDuration;
+    public $widgetOptions;
+    public $mediaIds;
+    public $audio;
+    public $permissions;
+    public $module;
+    public $name;
+    public $dataSetId;
+    public $updateInterval;
+    public $rowsPerPage;
+    public $showHeadings;
+    public $upperLimit;
+    public $lowerLimit;
+    public $filter;
+    public $ordering;
+    public $templateId;
+    public $overrideTemplate;
+    public $useOrderingClause;
+    public $useFilteringClause;
+    public $noDataMessage;
+
+    /**
+     * Get by Id
+     * @param $id
+     * @return $this|XiboDataSetView
+     * @throws XiboApiException
+     */
+    public function getById($id)
+    {
+        $response = $this->doGet('/playlist/widget', [
+            'playlistId' => $id
+        ]);
+
+        return clone $this->hydrate($response[0]);
+    }
+
+    /**
      * Create
      * @param $name
      * @param $dataSetId
@@ -28,5 +71,58 @@ class XiboDataSetView extends XiboEntity
 
         return $this->hydrate($response);
     }
-}
+    
+    /**
+     * Edit
+     * @param $dataSetColumnId
+     * @param $name
+     * @param duration
+     * @param updateInterval
+     * @param showHeadings
+     * @param upperLimit
+     * @param lowerLimit
+     * @param filter
+     * @param ordering
+     * @param templateId
+     * @param overrideTemplate
+     * @param useOrderingClause
+     * @param useFilteringClause
+     * @param noDataMessage
+     * @param $dataSetId
+     */
+    public function edit($dataSetColumnId, $name, $duration, $updateInterval, $rowsPerPage, $showHeadings, $upperLimit, $lowerLimit, $filter, $ordering, $templateId, $overrideTemplate, $useOrderingClause, $useFilteringClause, $noDataMessage, $widgetId)
+    {
+        $this->userId = $this->getEntityProvider()->getMe()->getId();
+        $this->dataSetColumnId = $dataSetColumnId;
+        $this->name = $name;
+        $this->duration = $duration;
+        $this->updateInterval = $updateInterval;
+        $this->rowsPerPage = $rowsPerPage;
+        $this->showHeadings = $showHeadings;
+        $this->upperLimit = $upperLimit;
+        $this->lowerLimit = $lowerLimit;
+        $this->filter = $filter;
+        $this->ordering = $ordering;
+        $this->templateId = $templateId;
+        $this->overrideTemplate = $overrideTemplate;
+        $this->useOrderingClause = $useOrderingClause;
+        $this->useFilteringClause = $useFilteringClause;
+        $this->noDataMessage = $noDataMessage;
+        $this->widgetId = $widgetId;
+        $response = $this->doPut('/playlist/widget/' . $widgetId, $this->toArray());
 
+        return $this->hydrate($response);
+
+    }
+
+    /**
+     * Delete
+     */
+    public function delete()
+    {
+        $this->userId = $this->getEntityProvider()->getMe()->getId();
+        $response = $this->doDelete('/playlist/widget/' . $this->widgetId , $this->toArray());
+
+        return true;
+    }
+}
